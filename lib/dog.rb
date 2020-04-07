9a46c8f3b5616efb4bbe36829446843082f2bd04
Adam Spencer <aspencer0919@gmail.com>
2020-04-07 16:26:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog
+  def name=(name)
+    @dog_name = name
+  end
+
+  def name
+    @dog_name
+  end
+
+  def bark=(woof)
+    @bark = woof
+  end
+
+  def bark
+    @bark
+  end
+end
